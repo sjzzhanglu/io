6c968287f3edf5f8f3fe32270a3d531221faece2
Unknown <sjzzhanglu@163.com>
2018-09-03 18:06:39 +0800

    学习中

---
+++ modules/cms/models/Post.php
@@ -0,0 +1,14 @@
+<?php namespace Cms\Models;
+
+use Model;
+
+/**
+ * Customization data used by a theme
+ *
+ * @package october\cms
+ * @author Alexey Bobkov, Samuel Georges
+ */
+class Post extends Model
+{
+
+}
